f33bfb70a8f3f6f0bb3b72033f1b6c770faa3a47
Yuri Cavalcante <yuri.cavalcante@edu.sc.senai.br>
2016-07-15 19:47:11 -0300

    criados metodos para inclusao e edição

--- WebContent/js/tarefa/tarefa-controller.js
+++ WebContent/js/tarefa/tarefa-controller.js
@@ -8,10 +8,31 @@ angular.module("tarefas").controller("TarefaController",function(){
 	};
 	
 	this.salvarTarefa = function(tarefa){
+		if(tarefa.id){
+			editarTarefa(tarefa);
+		} else{
+			incluirTarefa(tarefa);
+		}
+	};
+	
+	function incluirTarefa(tarefa){
 		tarefa.id = new Date().getTime();
 		this.tarefas.push(tarefa);
 		this.novaTarefa();
-	};
+	}
+	
+	function editarTarefa(tarefa){
+		var pos = -1;
+		angular.forEach(this.tarefas,function(item,index){
+			if(tarefa.id == item.id){
+				pos = index;
+			}
+		});
+		if(pos > -1){
+			this.tarefas.splice(pos,1,this.tarefa);
+			this.novaTarefa();
+		}
+	}
 	
 	this.removerTarefa = function(tarefa){
 		var pos = -1;
