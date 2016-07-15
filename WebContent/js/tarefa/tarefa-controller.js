bc19835d9800d90bd68c7ce32ab588c31cb29529
yuricavalcantesenai <yuri.cavalcante@edu.sc.senai.br>
2016-07-14 21:25:45 -0300

    implementada funcionalidade para incluir e listar tarefas

--- WebContent/js/tarefa/tarefa-controller.js
+++ WebContent/js/tarefa/tarefa-controller.js
@@ -1,3 +1,15 @@
 angular.module("tarefas").controller("TarefaController",function(){
 	this.tarefa = {};
+	
+	this.tarefas = [];
+	
+	this.novaTarefa = function(){
+		this.tarefa = {};
+	};
+	
+	this.salvarTarefa = function(tarefa){
+		tarefa.id = new Date().getTime();
+		this.tarefas.push(tarefa);
+		this.novaTarefa();
+	};
 });
