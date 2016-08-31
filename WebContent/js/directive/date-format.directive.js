7f3f438533b702cf86d23a114953a093d5dfdb9a
Yuri Cavalcante <yuri.cavalcante@edu.sc.senai.br>
2016-08-31 12:46:30 -0300

    incluído ngModel na diretiva

--- WebContent/js/directive/date-format.directive.js
+++ WebContent/js/directive/date-format.directive.js
@@ -8,7 +8,27 @@
 	function Directive(){
 		return {
 			restrict : 'A',
-			link: function(scope, element, attrs, controller){
+			require : 'ngModel',
+			link: function(scope, element, attrs, ngModel){
+				var dateViewFormat = /(\d{2})\/(\d{2})\/(\d{4})/;
+				var dateModelFormat = /(\d{4})-(\d{2})-(\d{2})/;
+				
+				ngModel.$formatters.push(formatter); // model -> view
+				ngModel.$parsers.push(parser); // view -> model
+				
+				function formatter(value){
+					if(dateModelFormat.test(value)){
+						return value.replace(dateModelFormat,"$3/$2/$1");
+					} return value;
+				}
+				
+				function parser(value){
+					if(dateViewFormat.test(value)){
+						return value.replace(dateViewFormat,"$3-$2-$1");
+					}
+					return null;
+				}
+				
 				element.on('keyup', function(e){
 					if(e.which >= 48 && e.which <= 57){
 						var value = e.target.value;
